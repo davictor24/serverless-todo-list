cb12ed556a950175284b4acb8b8e5ba8cc2141d7
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-10 00:38:40 +0100

    Implemented authoriser

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -6,6 +6,7 @@ import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import jwkToPem from 'jwk-to-pem'
 
 const logger = createLogger('auth')
 
@@ -54,11 +55,12 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  if (!jwt) throw new Error('Token is invalid')
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const res = await Axios.get(jwksUrl);
+  const pem = jwkToPem(res['data']['keys'][0])
+  var verifed = verify(token, pem, {algorithms: ['RS256']})
+  return verifed as JwtPayload
 }
 
 function getToken(authHeader: string): string {
