47dbb2ece020668f440f0290c1d51865ae65aba7
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-10 00:18:24 +0100

    Added resources to serverless.yaml and started authorisation implementation

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,8 +3,7 @@ const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'davictor.auth0.com', // Auth0 domain
+  clientId: 'rgtybNSwJ86X16m5EInFBHcJpMx72GHk', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
