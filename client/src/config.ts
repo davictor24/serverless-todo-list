d527156a209a98eb9abb782b4707cf6e845e7cfc
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-10 14:49:15 +0100

    Fixed CORS issues and linked frontend. It works!!!!!!!!!!!!

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,5 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'm474vwdy45'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'davictor.auth0.com', // Auth0 domain
